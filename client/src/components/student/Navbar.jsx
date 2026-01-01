9cb862d7da957c4eca7702a25fdd652e41582a1d
ABHI <abhi202864@gmail.com>
2026-01-01 23:54:38 +0530

    add home section code

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
@@ -8,13 +8,20 @@ const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
+  const navigate = useNavigate();
+
   return (
     <>
       <div
         className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 
           ${isCourseListPage ? "bg-white" : "bg-cyan-100/70"}`}
       >
-        <img src={assets.logo} alt="Logo" className="w-28 lg:w-32 cursor-pointer" />
+        <img
+          onClick={() => navigate("/")}
+          src={assets.logo}
+          alt="Logo"
+          className="w-28 lg:w-32 cursor-pointer"
+        />
         <div className="hidden md:flex items-center gap-5 text-gray-500">
           <div className="flex items-center gap-5">
             {user && (
